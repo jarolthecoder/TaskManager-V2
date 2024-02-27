ff06655bf8ccda1ac58307099ca97dfa3e2e8ae0
jarolthecoder <jarolr.webdev@gmail.com>
2024-02-27 09:09:03 -0600

    Added loading animations to app pages

---
+++ src/app/dashboard/tasks/loading.jsx
@@ -0,0 +1,9 @@
+import { Loader } from "@/components/shared";
+
+export default function Loading() {
+  return (
+    <div style={{ height: "calc(100vh - 150px)" }}>
+      <Loader />
+    </div>
+  );
+}
