3a93fe5fb03d987c7b058e9ef0bb4237c4f77984
jarolthecoder <jarolr.webdev@gmail.com>
2024-02-28 08:37:11 -0600

    Created theme toggle button component

---
+++ src/components/shared/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useContext } from "react";
+import { ThemeContext } from "@/context";
+import { IconButton, MatIcon } from "@/components/shared";
+
+export const ThemeToggle = () => {
+  const { toggleTheme, theme } = useContext(ThemeContext);
+  return (
+    <IconButton size="small" onClick={toggleTheme}>
+      <MatIcon
+        iconName={"light_mode"}
+        style={{
+          color: theme === "light" && "var(--clr-warning-main)",
+        }}
+      />
+    </IconButton>
+  );
+};
--- src/components/user/UserBar/UserBar.jsx
+++ src/components/user/UserBar/UserBar.jsx
@@ -1,24 +1,13 @@
-"use client"
-
-import { useContext } from "react";
-import { ThemeContext } from "@/context";
-import { IconButton, LanguageMenu, MatIcon, NotificationsMenu } from "@/components/shared";
+import { LanguageMenu, NotificationsMenu, ThemeToggle } from "@/components/shared";
 import { UserMenu } from "../UserMenu/UserMenu";
 import styles from "./UserBar.module.css";
 
 export const UserBar = () => {
- const { toggleTheme, theme } = useContext(ThemeContext);
+
   return (
     <div className={styles.main}>
       <LanguageMenu />
-      <IconButton size="small" onClick={toggleTheme}>
-        <MatIcon
-          iconName={"light_mode"}
-          style={{
-            color: theme === "light" && "var(--clr-warning-main)",
-          }}
-        />
-      </IconButton>
+      <ThemeToggle />
       <NotificationsMenu />
       <UserMenu />
     </div>
